526f912beccbbff63fd3bf6e83ae54b023362520
Toomore Chiang <toomore0929@gmail.com>
2015-06-20 13:07:58 +0000

    Add always yes.

--- install_mariadb.sh
+++ install_mariadb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #apt-get install python-software-properties
-apt-get install software-properties-common
+apt-get install -y software-properties-common
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/10.0/ubuntu trusty main'
 apt-get update
-apt-get install mariadb-server-10.0
+apt-get install -y mariadb-server-10.0
