59c0c56b8ea01dfbf094abc47bea0ae7665acc3c
Toomore Chiang <toomore0929@gmail.com>
2015-07-14 15:21:38 +0800

    Add more note in install mariadb.

--- install_deb/install_mariadb.sh
+++ install_deb/install_mariadb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Need swap
+# http://askubuntu.com/questions/286476/cant-install-mariadb-5-5-or-10-in-ubuntu-12-04-mysqld-failed-to-start
+# https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-12-04
+
 #apt-get install python-software-properties
 apt-get update
 apt-get install -y software-properties-common
