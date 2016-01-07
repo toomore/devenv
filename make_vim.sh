5143960584f880ff4f84b4b4eadd9bca1ce5e545
Toomore Chiang <toomore0929@gmail.com>
2016-01-07 07:27:08 +0000

    Tiny changed in make_vim.sh

--- make_vim.sh
+++ make_vim.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-VIMVER="7.4.1051"
+VIMVER="7.4.1055"
 
 curl -L https://github.com/vim/vim/archive/v${VIMVER}.tar.gz > vim.tar.gz
 tar -xvf ./vim.tar.gz
 cd ./vim-${VIMVER}/
 
-./configure --with-features=huge --enable-luainterp                    \
+./configure --with-features=huge --enable-luainterp --enable-multibyte \
+            --with-tlib=ncurses --enable-cscope                        \
             --enable-gui=no --without-x --prefix=/usr               && \
 make VIMRUNTIMEDIR=/usr/share/vim/vim74                             && \
 make install
